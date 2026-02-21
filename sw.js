7f74344fe0d08cf03a90c294be69405c9ae93dd1
saikelengineering-code <saikel.engineering@gmail.com>
2026-02-21 09:12:05 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'saikel-v10';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
